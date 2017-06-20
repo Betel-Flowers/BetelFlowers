c0bc4314484f920ac5aa574a0f2c4b10ad512d38
ivan16luis <ivanlugiz16@yahoo.com>
2017-06-20 17:01:51 -0500

    cambios 20/06/2017  detalis

--- BetelFlowers-ejb/src/main/java/com/betel/flowers/service/OpcionSistemaService.java
+++ BetelFlowers-ejb/src/main/java/com/betel/flowers/service/OpcionSistemaService.java
@@ -73,6 +73,7 @@ public class OpcionSistemaService implements Serializable {
         for (OpcionSistema opc : list) {
             urls.add(opc.getMenuitem_outcome() + ".xhtml");
         }
+        urls.add("/faces/views/venta.xhtml");
         urls.add("/faces/views/perfil.xhtml");
         urls.add("/faces/views/usuario.xhtml");
         urls.add("/faces/views/tipousuario.xhtml");
